bd4a3cce2b07f16426355c04f6d9640e2d99c956
fresnel25 <ngaleufresnel@gmail.com>
2025-11-07 09:34:50 +0100

    ajout de la fonctionnalité AddTodo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-function createTodo(id, text, Priorite) {
+let Todo = (id, text, Priorite) => {
   return { id, text, Priorite };
-}
+};
 
 function App() {
-  const [input, setInput] = useState("")
-   const [priorite, setPriorite] = useState("Moyenne")
+  const [input, setInput] = useState("");
+  const [priorite, setPriorite] = useState("Moyenne");
+  const savedTodos = localStorage.getItem("todos");
+  const initialTodos = savedTodos ? JSON.parse(savedTodos) : [];
+  const [todos, setTodos] = useState([initialTodos]);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos]);
+
+  let addTodo = () => {
+    if (input.trim() == "") {
+      return;
+    }
+    let newTodo = Todo(Date.now(), input, priorite);
+
+    const newTodos = [newTodo, ...todos];
+    setTodos(newTodos);
+    setInput("");
+    setPriorite("Moyenne");
+
+    console.log(newTodos);
+  };
+
   return (
     <div className="flex justify-center">
       <div className="w-2/3 flex flex-col gap-4 my-15 bg-base-300 p-5 rounded-2xl">
@@ -16,14 +38,20 @@ function App() {
             className="input w-full"
             placeholder="Ajouter une tâche..."
             value={input}
-            onChange={(e)=> setInput(e.target.value)}
+            onChange={(e) => setInput(e.target.value)}
           />
-          <select className="select w-full" value={priorite} onChange={(e)=> setPriorite(e.target.value)}>
+          <select
+            className="select w-full"
+            value={priorite}
+            onChange={(e) => setPriorite(e.target.value)}
+          >
             <option value="Urgente">Urgente</option>
             <option value="Moyenne">Moyenne</option>
             <option value="Basse">Basse</option>
           </select>
-          <button className="btn btn-primary">Ajouter</button>
+          <button onClick={addTodo} className="btn btn-primary">
+            Ajouter
+          </button>
         </div>
       </div>
     </div>
